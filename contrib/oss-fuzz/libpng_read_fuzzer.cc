b533594be7fe77512dc80cbf366fbe5b81516e98
Tian Qiu <qiutianchloe@hotmail.com>
2024-09-24 17:46:58 +1000

    change and try set_rgb and delete graytorgb mode again and fix a bug

--- contrib/oss-fuzz/libpng_read_fuzzer.cc
+++ contrib/oss-fuzz/libpng_read_fuzzer.cc
@@ -180,22 +180,11 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   }
 
   // Set several transforms that browsers typically use:
-  png_set_gray_to_rgb(png_handler.png_ptr);
   png_set_expand(png_handler.png_ptr);
   png_set_packing(png_handler.png_ptr);
   png_set_scale_16(png_handler.png_ptr);
   png_set_tRNS_to_alpha(png_handler.png_ptr);
-
-  png_set_alpha_mode(png_handler.png_ptr, PNG_ALPHA_OPTIMIZED, PNG_DEFAULT_sRGB);  
-  png_color_16 back;
-    // use a default value
-  png_uint_16 default_red = 6968;
-  png_uint_16 default_green = 23434;
-  png_uint_16 default_blue = 2366;
-  back.red = default_red;
-  back.green = default_green;
-  back.blue = default_blue;
-  png_set_background(png_handler.png_ptr, &back, PNG_BACKGROUND_GAMMA_FILE, 1, 1.0);
+  png_set_rgb_to_gray(png_handler.png_ptr, 1, 6968 / 32768., 23434 / 32768.);
 
   int passes = png_set_interlace_handling(png_handler.png_ptr);
 

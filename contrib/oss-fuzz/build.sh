e805b1401f0d83c4f182615fdd8a0c4c7767bf9d
Tian Qiu <39074976+qiutianchloe@users.noreply.github.com>
2024-09-28 16:51:27 +1000

    Update build.sh

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -43,11 +43,11 @@ $CXX $CXXFLAGS -std=c++11 -I. \
      -lFuzzingEngine .libs/libpng16.a -lz
 
 # add seed corpus.
- find $SRC/libpng -name "*.png" | grep -v crashers | \
+find $SRC/libpng -name "*.png" | grep -v crashers | \
       xargs zip $OUT/libpng_read_fuzzer_seed_corpus.zip
 
 # add more than 8000 public corpus
-#zip -j $OUT/libpng_read_fuzzer_seed_corpus.zip $SRC/libpng/contrib/oss-fuzz/libpng_read_fuzzer/*
+# zip -j $OUT/libpng_read_fuzzer_seed_corpus.zip $SRC/libpng/contrib/oss-fuzz/libpng_read_fuzzer/*
 
 
 cp $SRC/libpng/contrib/oss-fuzz/*.dict \
